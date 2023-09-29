5555e88a4451562ba29f15edfc9f981a5341d65c
Abdulaziz <abdusattorov2004@mail.ru>
2023-09-29 11:11:17 +0500

    sendign location and contact num

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from keyboards.default.start import MenuStart
 
 from loader import dp
 
@@ -13,4 +14,5 @@ async def bot_start(message: types.Message):
 @dp.message_handler(commands='start')
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Salom, {message.from_user.full_name}!\n")
+    await message.answer("telefon va manzilingizni yuboring", reply_markup=MenuStart)
